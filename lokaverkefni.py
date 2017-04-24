883b13b80905b92e9ece6ca5f197507b6ea9c795
sigthor420 <sigthorge244@nemi.tskoli.is>
2017-04-24 17:42:49 +0000

    daaaaaamn daniel
    schlong

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -1,18 +1,29 @@
-class kind:
-    def __self__(self, nafn, tyngd, mjokl, ull, fjoldiafkvaema, laeri, frjosemi, bakvodvi, malir, papi, madre):
-        self.nafn = nafn
-        self.tyngd = tyngd
-        self.mjokl = mjokl
-        self.ull = ull
-        self.afkvaemi = fjoldiafkvaema
-        self.laeri = laeri
-        self.frjosemi = frjosemi
-        self.bakvodvi = bakvodvi
-        self.malir = malir
-        self.papi = papi
-        self.madre = madre
+class Kind:
+    def __self__(self, _nafn, _tyngd, _mjokl, _ull, _fjoldiafkvaema, _laeri, _frjosemi, _bakvodvi, _malir, _papi, _madre):
+        self.nafn = _nafn
+        self.tyngd = _tyngd
+        self.mjokl = _mjokl
+        self.ull = _ull
+        self.afkvaemi = _fjoldiafkvaema
+        self.laeri = _laeri
+        self.frjosemi = _frjosemi
+        self.bakvodvi = _bakvodvi
+        self.malir = _malir
+        self.papi = _papi
+        self.madre = _madre
 
 listi1 = []
 listi2 = []
 
+skra = []
+hermadur = Kind
+print (hermadur)
+with open("new1.txt", "r", encoding="utf-8") as f:
+    for i in f.readlines():
+        a = i.split(",")
+        print(a)
+        print(i)
+        skra.append(hermadur(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10]))
+
+print(skra[3].mjokl)
 print("Velkominn í örugglega leiðinlegasta leik sem þú munt nokkurn tíman spila.")
