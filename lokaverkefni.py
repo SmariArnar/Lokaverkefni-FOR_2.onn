f83aecfea158616fd9601dc5f581388e3cce57a9
sigthor420 <sigthorge244@nemi.tskoli.is>
2017-04-24 16:46:26 +0000

    succ ma anaconda
    do it

---
+++ lokaverkefni.py
@@ -0,0 +1,18 @@
+class kind:
+    def __self__(self, nafn, tyngd, mjokl, ull, fjoldiafkvaema, laeri, frjosemi, bakvodvi, malir, papi, madre):
+        self.nafn = nafn
+        self.tyngd = tyngd
+        self.mjokl = mjokl
+        self.ull = ull
+        self.afkvaemi = fjoldiafkvaema
+        self.laeri = laeri
+        self.frjosemi = frjosemi
+        self.bakvodvi = bakvodvi
+        self.malir = malir
+        self.papi = papi
+        self.madre = madre
+
+listi1 = []
+listi2 = []
+
+print("Velkominn í örugglega leiðinlegasta leik sem þú munt nokkurn tíman spila.")
