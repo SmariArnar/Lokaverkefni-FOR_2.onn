74229b35d791780f33ccb74823d64d05a16c1969
sigthor420 <sigthorge244@nemi.tskoli.is>
2017-04-26 10:55:29 +0000

    Add files via upload

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -1,21 +1,28 @@
 class Kind:
-    def __init__(self, nafn, tyngd, mjokl, ull, fjoldiafkvaema, laeri, frjosemi, bakvodvi, malir, papi, madre):
-        self.nafn = nafn
-        self.tyngd = tyngd
-        self.mjokl = mjokl
-        self.ull = ull
-        self.afkvaemi = fjoldiafkvaema
-        self.laeri = laeri
-        self.frjosemi = frjosemi
-        self.bakvodvi = bakvodvi
-        self.malir = malir
-        self.papi = papi
-        self.madre = madre
-listi = []
-with open("new1.txt","r", encoding="utf-8") as open_file:
-    for i in open_file.readlines():
+    def __init__(self, _nafn, _tyngd, _mjokl, _ull, _fjoldiafkvaema, _laeri, _frjosemi, _bakvodvi, _malir, _papi, _madre):
+        self.nafn = _nafn
+        self.tyngd = _tyngd
+        self.mjokl = _mjokl
+        self.ull = _ull
+        self.afkvaemi = _fjoldiafkvaema
+        self.laeri = _laeri
+        self.frjosemi = _frjosemi
+        self.bakvodvi = _bakvodvi
+        self.malir = _malir
+        self.papi = _papi
+        self.madre = _madre
+
+listi1 = []
+listi2 = []
+
+skra = []
+with open("saltedpeanuts.txt", "r", encoding="utf-8") as f:
+    for i in f.readlines():
         a = i.split(",")
-        listi.append(Kind(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10]))
-    for i in listi:
-        print(i.nafn,end=" ")
-        print(i.tyngd)
+        if "\n" in a[10]:
+            a[10] = a[10].replace("\n", "")
+        skra.append(Kind(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10]))
+print("Velkominn í örugglega leiðinlegasta leik sem þú munt nokkurn tíman spila.")
+
+while True:
+    print("Y O W H A T S U P J I M B O")
