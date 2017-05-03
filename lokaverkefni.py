90edfe79db06fd6f2f2e0c2921010acc10c12285
kormakurun285 <kormakurun285@nemi.tskoli.is>
2017-05-03 12:33:26 +0000

    minor changes

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -30,7 +30,6 @@ def Deal(_spilarar):
             spilarar[tel].append(spil.pop(randomTala))
         else:
             break
-        print(tel)
         if tel >= spilararSpurning - 1:
             tel = 0
         else:
@@ -44,7 +43,7 @@ def Turn(_spilari, _numerADoti):
         if _spilari.listi[_numerADoti] < spilarar[i][0].listi[_numerADoti]:
             sigurvegari = i
         elif _spilari.listi[_numerADoti] == spilarar[i][0].listi[_numerADoti]:
-            afgangur.append()#dabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdabdab
+            afgangur.append()
 
 spil = []
 with open("spilastokkur.txt", "r", encoding="utf-8") as f:
@@ -53,16 +52,54 @@ with open("spilastokkur.txt", "r", encoding="utf-8") as f:
         if "\n" in a[10]:
             a[10] = a[10].replace("\n", "")
         spil.append(Kind(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10]))
-print("Velkominn í örugglega leiðinlegasta leik sem þú munt nokkurn tíman spila.")
+#Leikreglur eru settar í þennan allt of langa texta
+print("\nLEIKREGLUR")
+print("\n\tLeikurinn, eins og flestir leikir, gengur út á það að sigra.")
+print("\tSá leikmaður hlotnast sigur sem eftir stendur þegar hinu/m leikmönnum er tortýmt.")
+print("\tSpilunum 52 er skipt á milli leikmanna, sama hversu margir þeir eru.")
+print("\tAllir leikmenn draga spil og sá sem á leikinn velur eiginleika,")
+print("\tsíðan bera allir leikmenn saman töluna undir eiginleikanum sem varð fyrir valinu.")
+print("\n\tÞyngd í kílóum = 1\t\tMjólkurlagni dætra = 2\t\tEinkunn ullar = 3\t\t\t\tFjöldi afkvæma = 4")
+print("\n\tEinkunn læris = 5\t\tFrjósemi = 6\t\t\t\tGerð / þykkt bakvöðva = 7\t\tEinkunn fyrir malir = 8")
 
 gera = 0
 
 while True:
-    print("Y O W H A T S U P J I M B O")
-    spilararSpurning = int(input("Hversu margir eru að spila?" ))
+    #Notandinn er spurður hversu marga leikmenn hann vill hafa.
+    spilararSpurning = int(input("\n\nHversu margir eru að spila?" ))
     Deal(spilararSpurning)
-    print("Nú hefst leikurinn!")
+    print("\nNú hefst leikurinn!\n")
     while True:
+        bil19 = "                   "
+        bil6 = "        "
+
+        print("╭━━━━━━━━━━━━━━━━━━━━━━━━ ━━━━━━━━━━━╮")
+        print("┃   ┌──╮                ╭──┐  ┃")
+        print("┃   │┌─┴────────────┴─┐│  ┃")
+        print("┃   ││" + bil19 + "  ││  ┃")
+        print("┃   └┤" + bil19 + "   ├┘  ┃")
+        print("┃ ╭──╰─────── ─────────╯  ─╮ ┃")
+        print("┃ │                  ,@;@,    │  ┃")
+        print("┃ │     ,@;@;@;@;@;@/ )@;@;   │  ┃")
+        print("┃ │   ,;@;@;@;@;@;@|_/@' e\   │  ┃")
+        print("┃ │  (|@;@:@\@;@;@;@:@(    \  │  ┃")
+        print("┃ │    '@;@;@|@;@;@;@;'`' - ' │  ┃")
+        print("┃ │     '@;@;/;@;/;@;'        │  ┃")
+        print("┃ │      ) //   | ||          │  ┃")
+        print("┃ │      \ \ \   | ||         │  ┃")
+        print("┃ │       \ \ \  ) \ \        │  ┃")
+        print("┃ │        `''' `''``         │  ┃")
+        print("┃ ├ ─── ─┬────  ┬─ ──  ─┬──── ┤  ┃")
+        print("┃ │" + bil6 + "│" + bil6 + "│" + bil6 + "│" + bil6 + "│ ┃")
+        print("┃ │" + bil6 + "│" + bil6 + "│" + bil6 + "│" + bil6 + "│ ┃")
+        print("┃ ├───  ─┼────  ┼─ ──  ─┼────┤ ┃")
+        print("┃ │" + bil6 + "│" + bil6 + "│" + bil6 + "│" + bil6 + "│ ┃")
+        print("┃ │" + bil6 + "│" + bil6 + "│" + bil6 + "│" + bil6 + "│ ┃")
+        print("┃ ╰──╭──┴──  ──┴── ── ┴ ──╮──╯ ┃")
+        print("┃   ┌┤" + bil19 + "  ├┐   ┃")
+        print("┃   ││" + bil19 + "  ││   ┃")
+        print("┃   │└┬──────────────┬┘│   ┃")
+        print("┃   └─╯                    ╰─┘   ┃")
+        print("╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯")
         numerADotiTilAdNotaTilTessAdBeraSamanVidOnnurDotHjaOdrumSpilurum = input("Hvað viltu gera? ")
         Turn(spilarar[gera], numerADotiTilAdNotaTilTessAdBeraSamanVidOnnurDotHjaOdrumSpilurum)
-        print("")
